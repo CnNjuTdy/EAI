39b70059f1812edafa4c17b01db37769240d370e
Tondiyee <tdy_nju@163.com>
2017-06-10 20:51:43 +0800

    8:51

--- Project/src/main/java/spider/GewaraSpider.java
+++ Project/src/main/java/spider/GewaraSpider.java
@@ -31,7 +31,7 @@ public class GewaraSpider {
     @Autowired
     private GewalaPlanDao dao3;
 
-    private final static String URL="http://www.gewara.com/movie/ajax/getOpiItemNew.xhtml";
+    private final static String URL = "http://www.gewara.com/movie/ajax/getOpiItemNew.xhtml";
 
     /*
     参数
@@ -39,25 +39,25 @@ public class GewaraSpider {
     fyrq:放映日期
     cid:影院ID
      */
-    public static List<GewalaPlan> getData(HashMap<String,String> parms) throws IOException {
+    public static List<GewalaPlan> getData(HashMap<String, String> parms) throws IOException {
 
-        List<GewalaPlan> result=new ArrayList<>();
+        List<GewalaPlan> result = new ArrayList<>();
         Document doc = Jsoup.connect(URL).header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2").data(parms).get();
-        Elements es=doc.getElementsByClass("chooseOpi_body");
-        Elements lis=es.select("ul").get(0).select("li");
-       String cName=doc.getElementById("opiCinemaDetail").select("a").get(0).text();
+        Elements es = doc.getElementsByClass("chooseOpi_body");
+        Elements lis = es.select("ul").get(0).select("li");
+        String cName = doc.getElementById("opiCinemaDetail").select("a").get(0).text();
 
-        for (Element li:lis
+        for (Element li : lis
                 ) {
-            GewalaPlan gewalaPlan=new GewalaPlan();
+            GewalaPlan gewalaPlan = new GewalaPlan();
             gewalaPlan.setcId(Integer.parseInt(parms.get("cid")));
             gewalaPlan.setmId(Integer.parseInt(parms.get("movieid")));
             gewalaPlan.setcName(cName);
             gewalaPlan.setDate(parms.get("fyrq"));
 
-            Elements ems=li.select("em");
-            Elements labels=li.select("label");
-            Elements bs=li.select("b");
+            Elements ems = li.select("em");
+            Elements labels = li.select("label");
+            Elements bs = li.select("b");
 
             gewalaPlan.setStart(bs.get(0).text());
             gewalaPlan.setEnd(ems.get(0).text());
@@ -65,26 +65,26 @@ public class GewaraSpider {
             gewalaPlan.setTh(labels.get(0).text());
             result.add(gewalaPlan);
         }
-      return result;
+        return result;
     }
 
 
     public void getPlans() throws IOException {
         List<Cinema> cinemas = dao1.findAll();
         List<Movie> movies = dao2.findAll();
-        for (Cinema cinema:cinemas){
-            for(Movie movie:movies){
+        for (Cinema cinema : cinemas) {
+            for (Movie movie : movies) {
                 HashMap<String, String> params = new HashMap<>();
-                params.put("movieid", movie.getGewalaId()+"");
+                params.put("movieid", movie.getGewalaId() + "");
                 params.put("fyrq", DateUtil.get2Day());//
-                params.put("cid", cinema.getGewalaId()+"");
+                params.put("cid", cinema.getGewalaId() + "");
 
 
-               List<GewalaPlan> list= getData(params);
+                List<GewalaPlan> list = getData(params);
                 // TODO: 2017/6/10  doc转plan
-for(GewalaPlan ge:list)
-                System.out.println(ge.toString());
-              //  dao3.save(plan);
+                for (GewalaPlan ge : list)
+                    System.out.println(ge.toString());
+                //  dao3.save(plan);
 
             }
         }
