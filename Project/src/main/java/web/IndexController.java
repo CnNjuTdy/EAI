0e882cd0ed3e9c5fe8fe0929d5005fffa2c6a6e8
csy14 <141250005@smail.nju.edu.cn>
2017-06-11 13:03:26 +0800

    111
    111

--- Project/src/main/java/web/IndexController.java
+++ Project/src/main/java/web/IndexController.java
@@ -4,18 +4,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import spider.GewaraSpider;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * Created by Tondiyee on 2017/6/1.
  */
 @Controller
-@RequestMapping("/top")
+//@RequestMapping("/top")
 public class IndexController  {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @Resource(name = "GewaraSpider")
+    GewaraSpider gewaraSpider;
+
+    @RequestMapping(value = "/home",method = RequestMethod.GET)
     public ModelAndView index(){
+
         return new ModelAndView("index");
     }
 
+    @RequestMapping(value = "/wonder",method = RequestMethod.GET)
+    public ModelAndView wonder(HttpServletRequest request){
+        String wonder = request.getParameter("wonder");
+        try {
+            gewaraSpider.getPlans();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ModelAndView("wonder");
+    }
 
 }
--- Project/src/main/webapp/views/home.jsp
+++ Project/src/main/webapp/views/home.jsp
@@ -1,4 +1,11 @@
-<!DOCTYPE html>
+<%--
+  Created by IntelliJ IDEA.
+  User: caosh
+  Date: 2017/6/11
+  Time: 12:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <meta charset="utf-8">
