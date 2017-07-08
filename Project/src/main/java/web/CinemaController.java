175f8525cf40d94390f912440692094f75976c7e
JohnsonFu <fulinhua@fulinhuadeMacBook-Pro.local>
2017-07-08 14:31:28 +0800

    test

--- Project/src/main/java/web/CinemaController.java
+++ Project/src/main/java/web/CinemaController.java
@@ -1,15 +1,10 @@
 package web;
 
-import entity.UniformPlan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import service.SearchCinemaService;
-
-import java.util.List;
 
 /**
  * Created by fulinhua on 2017/6/11.
@@ -25,34 +20,10 @@ public class CinemaController {
         return "CinemaHome";
     }
 
-    @Autowired
-    SearchCinemaService searchCinemaService;
 
     @RequestMapping(value="/getPlan")
     public String getData(ModelMap model, String cinemaName){
-//        System.out.println("hhhhhhhhh");
-        System.out.println(cinemaName);
-//        String movieName = request.getParameter("wonder");
-        List<UniformPlan> datalist=searchCinemaService.getPlansByCinema(cinemaName);
-        model.addAttribute("datalist",datalist);
-//        model.put("datalist",datalist);
-        System.out.println(datalist.size());
-//        int ume=0,jinyi=0,lu=0,peace=0;
-//        for (int i=0;i<datalist.size();i++) {
-//            switch (datalist.get(i).getcName()){
-//                case "UME国际影城(玄武门店)":ume++;break;
-//                case "金逸影城(大观店)":jinyi++;break;
-//                case "卢米埃影城(紫峰店)":lu++;break;
-//                case "和平影视娱乐城":peace++;break;
-//                default:break;
-//            }
-//            System.out.println(datalist.get(i));
-//        }
-//        System.out.println(ume+" " +jinyi+" "+lu+" "+peace+" ");
-//        model.addAttribute("ume",ume);
-//        model.addAttribute("jinyi",jinyi);
-//        model.addAttribute("lu",lu);
-//        model.addAttribute("peace",peace);
+//
         return "showCinemaData";
     }
 
--- Project/src/test/MyTest.java
+++ Project/src/test/MyTest.java
@@ -1,17 +1,10 @@
 import dao.BaseDao;
-import dao.CinemaDao;
-import entity.UniformPlan;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import service.SearchCinemaService;
-import service.SearchMovieService;
-import spider.GewaraSpider;
-import spider.MaoyanSpider;
-import spider.TaopiaopiaoSpider;
 
 import java.io.IOException;
 import java.util.List;
